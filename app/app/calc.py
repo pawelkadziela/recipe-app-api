0a5a6594ade7cfc2875cfc844c82c29f00d216c7
pawelkadziela <kadzielapawel87@gmail.com>
2023-09-24 00:38:51 +0200

    postgresql installation added to dockerfile

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+def add(x , y):
+    """Adds x and y and returns result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtracts x from y and returns result."""
+
+    return y - x
